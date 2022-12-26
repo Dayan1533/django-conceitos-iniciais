65305740e45f61aa50de8322be58d077ec274b79
Dayan1533 <dayanramos5537@gmail.com>
2022-12-25 23:46:01 -0300

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dayan/Documentos/Programação/Django/django-conceitos-iniciais/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
