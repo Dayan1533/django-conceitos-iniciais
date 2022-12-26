6c8e17c011188d59ded32cd3564572d9e3d13e17
Dayan_Gomes <51213795+Dayan1533@users.noreply.github.com>
2022-12-26 01:30:45 -0300

    Delete venv directory

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/dayan/Documentos/Programação/Django/django-conceitos-iniciais/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
